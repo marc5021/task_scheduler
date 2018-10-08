b504c74d1f24399847275b009b557cf0bbef9d92
Marcus <marcus.hs2000@gmail.com>
2018-10-08 19:08:08 +0200

    yearweek group

---
+++ src/app/pipes/array-keys.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ArrayKeysPipe } from './array-keys.pipe';
+
+describe('ArrayKeysPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ArrayKeysPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
